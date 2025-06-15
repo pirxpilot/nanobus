47f2c5fd8f5995072db0a14b61a7d578f9902dac
Damian Krzeminski <pirxpilot@furkot.com>
2025-06-15 11:05:27 -0600

    replace `tape` with `node:test`

--- test.js
+++ test.js
@@ -1,75 +1,73 @@
-const tape = require('tape');
+const test = require('node:test');
 const nanobus = require('./');
-
-/* global globalThis */
 
 globalThis.DEBUG = true;
 
-tape('nanobus', function (t) {
-  t.test('should assert input types', function (t) {
+test('nanobus', async t => {
+  await t.test('should assert input types', t => {
     t.plan(11);
     const bus = nanobus();
-    t.throws(bus.emit.bind(bus), /string/);
-    t.throws(bus.on.bind(bus), /string/);
-    t.throws(bus.on.bind(bus, 'foo'), /function/);
-    t.throws(bus.once.bind(bus), /string/);
-    t.throws(bus.once.bind(bus, 'foo'), /function/);
-    t.throws(bus.removeListener.bind(bus), /string/);
-    t.throws(bus.removeListener.bind(bus, 'foo'), /function/);
+    t.assert.throws(bus.emit.bind(bus), /string/);
+    t.assert.throws(bus.on.bind(bus), /string/);
+    t.assert.throws(bus.on.bind(bus, 'foo'), /function/);
+    t.assert.throws(bus.once.bind(bus), /string/);
+    t.assert.throws(bus.once.bind(bus, 'foo'), /function/);
+    t.assert.throws(bus.removeListener.bind(bus), /string/);
+    t.assert.throws(bus.removeListener.bind(bus, 'foo'), /function/);
 
     const s = Symbol('event');
     const fn = function () {};
-    t.doesNotThrow(bus.emit.bind(bus, s));
-    t.doesNotThrow(bus.on.bind(bus, s, fn));
-    t.doesNotThrow(bus.once.bind(bus, s, fn));
-    t.doesNotThrow(bus.removeListener.bind(bus, s, fn));
+    t.assert.doesNotThrow(bus.emit.bind(bus, s));
+    t.assert.doesNotThrow(bus.on.bind(bus, s, fn));
+    t.assert.doesNotThrow(bus.once.bind(bus, s, fn));
+    t.assert.doesNotThrow(bus.removeListener.bind(bus, s, fn));
   });
 
-  t.test('should emit messages', function (t) {
+  await t.test('should emit messages', t => {
     t.plan(4);
     const bus = nanobus();
     const obj = { bin: 'baz' };
     bus.on('foo:bar', function (data) {
-      t.equal(data, obj, 'data was same');
+      t.assert.equal(data, obj, 'data was same');
     });
 
     bus.emit('foo:bar', obj);
 
     bus.on('beep:boop', function (data) {
-      t.equal(data, undefined);
+      t.assert.equal(data, undefined);
     });
 
     bus.emit('beep:boop');
 
     bus.on('baz:floop', function (arg1, arg2) {
-      t.equal(arg1, 'arg1', 'data was same');
-      t.equal(arg2, 'arg2', 'data was same');
+      t.assert.equal(arg1, 'arg1', 'data was same');
+      t.assert.equal(arg2, 'arg2', 'data was same');
     });
 
     bus.emit('baz:floop', 'arg1', 'arg2');
   });
 
-  t.test('should prepend listeners', function (t) {
+  await t.test('should prepend listeners', t => {
     t.plan(4);
     let i = 0;
     const bus = nanobus();
     bus.on('foo:bar', function (_data) {
-      t.equal(i, 1);
+      t.assert.equal(i, 1);
     });
 
     bus.prependListener('foo:bar', function (_data) {
-      t.equal(i, 0);
+      t.assert.equal(i, 0);
       i++;
     });
 
     bus.emit('foo:bar');
 
     bus.on('*', function (_data) {
-      t.equal(i, 1);
+      t.assert.equal(i, 1);
     });
 
     bus.prependListener('*', function (_eventName, data) {
-      t.equal(i, data);
+      t.assert.equal(i, data);
       i++;
     });
 
@@ -77,16 +75,16 @@ tape('nanobus', function (t) {
     bus.emit('bar:baz', i);
   });
 
-  t.test('should prepend once listeners', function (t) {
+  await t.test('should prepend once listeners', t => {
     t.plan(3);
     let i = 0;
     const bus = nanobus();
     bus.on('foo:bar', function (_data) {
-      t.equal(i, 1);
+      t.assert.equal(i, 1);
     });
 
     bus.prependOnceListener('foo:bar', function (_data) {
-      t.equal(i, 0);
+      t.assert.equal(i, 0);
       i++;
     });
 
@@ -94,18 +92,18 @@ tape('nanobus', function (t) {
     bus.emit('foo:bar');
   });
 
-  t.test('should emit messages once', function (t) {
+  await t.test('should emit messages once', t => {
     t.plan(1);
     const bus = nanobus();
     bus.once('foo:bar', function (_data) {
-      t.pass('called');
+      t.assert.ok('called');
     });
 
     bus.emit('foo:bar');
     bus.emit('foo:bar');
   });
 
-  t.test('should properly emit messages when using both once() and on() ', function (t) {
+  await t.test('should properly emit messages when using both once() and on() ', t => {
     t.plan(2);
     const bus = nanobus();
     let i = 0;
@@ -127,32 +125,32 @@ tape('nanobus', function (t) {
     });
 
     bus.emit('foo:bar');
-    t.equal(i, 4, 'incremented by once() and on()');
+    t.assert.equal(i, 4, 'incremented by once() and on()');
 
     bus.emit('foo:bar');
-    t.equal(i, 6, 'incremented by on() only');
+    t.assert.equal(i, 6, 'incremented by on() only');
   });
 
-  t.test('should trigger wildcard once', function (t) {
+  await t.test('should trigger wildcard once', t => {
     t.plan(3);
     const bus = nanobus();
     bus.once('*', function (_data) {
-      t.pass('called');
+      t.assert.ok('called');
     });
 
     bus.on('foo:bar', function (_data) {
-      t.pass('called foo:bar');
+      t.assert.ok('called foo:bar');
     });
 
     bus.on('foo:baz', function (_data) {
-      t.pass('called foo:baz');
+      t.assert.ok('called foo:baz');
     });
 
     bus.emit('foo:bar');
     bus.emit('foo:baz');
   });
 
-  t.test('should be able to remove listeners', function (t) {
+  await t.test('should be able to remove listeners', t => {
     t.plan(3);
     const bus = nanobus();
     bus.on('foo:bar', goodHandler);
@@ -165,11 +163,11 @@ tape('nanobus', function (t) {
     bus.emit('foo:bar');
 
     function goodHandler(_data) {
-      t.pass('called');
+      t.assert.ok('called');
     }
 
     function onceHandler(_data) {
-      t.pass('called');
+      t.assert.ok('called');
     }
 
     function badHandler(_data) {
@@ -177,7 +175,7 @@ tape('nanobus', function (t) {
     }
   });
 
-  t.test('should be able to remove all listeners', function (t) {
+  await t.test('should be able to remove all listeners', t => {
     t.plan(1);
     const bus = nanobus();
     let i = 0;
@@ -188,14 +186,14 @@ tape('nanobus', function (t) {
     bus.emit('foo:bar');
     bus.emit('bin:baz');
 
-    t.equal(i, 0, 'no events called');
+    t.assert.equal(i, 0, 'no events called');
 
     function handler(_data) {
       i++;
     }
   });
 
-  t.test('should be able to remove all listeners for an event', function (t) {
+  await t.test('should be able to remove all listeners for an event', t => {
     t.plan(1);
     const bus = nanobus();
     let i = 0;
@@ -206,14 +204,14 @@ tape('nanobus', function (t) {
     bus.emit('foo:bar');
     bus.emit('bin:baz');
 
-    t.equal(i, 1, '1 event called');
+    t.assert.equal(i, 1, '1 event called');
 
     function handler(_data) {
       i++;
     }
   });
 
-  t.test('should be able to have * listeners', function (t) {
+  await t.test('should be able to have * listeners', t => {
     t.plan(12);
     const bus = nanobus();
     let i = 0;
@@ -223,42 +221,42 @@ tape('nanobus', function (t) {
     bus.on('*', handler);
 
     bus.emit('foo:bar');
-    t.equal(i, 2, 'count 2');
+    t.assert.equal(i, 2, 'count 2');
 
     bus.emit('bin:baz');
-    t.equal(i, 4, 'count 4');
+    t.assert.equal(i, 4, 'count 4');
 
     bus.removeAllListeners('bin:baz');
     bus.emit('bin:baz');
-    t.equal(i, 5, 'count 5');
+    t.assert.equal(i, 5, 'count 5');
 
     bus.removeListener('*', handler);
     bus.emit('foo:bar');
-    t.equal(i, 6, 'count 6');
+    t.assert.equal(i, 6, 'count 6');
 
     bus.on('*', handler);
     bus.emit('foo:bar');
-    t.equal(i, 8, 'count 8');
+    t.assert.equal(i, 8, 'count 8');
 
     bus.removeAllListeners('*');
     bus.emit('foo:bar');
-    t.equal(i, 9, 'count 9');
+    t.assert.equal(i, 9, 'count 9');
 
     bus.on('*', handler);
     bus.emit('foo:bar');
-    t.equal(i, 11, 'count 11');
+    t.assert.equal(i, 11, 'count 11');
 
     bus.removeAllListeners();
-    t.equal(i, 11, 'count 11');
+    t.assert.equal(i, 11, 'count 11');
 
     bus.once('*', handler);
     bus.emit('foo:bar');
-    t.equal(i, 12, 'count 12');
+    t.assert.equal(i, 12, 'count 12');
     bus.emit('foo:bar');
-    t.equal(i, 12, 'count 12');
+    t.assert.equal(i, 12, 'count 12');
 
     bus.removeAllListeners();
-    t.equal(i, 12, 'count 12');
+    t.assert.equal(i, 12, 'count 12');
 
     bus.on('*', starHandler);
     bus.emit('star:event', i);
@@ -269,69 +267,68 @@ tape('nanobus', function (t) {
     }
 
     function starHandler(_eventName, data) {
-      t.equal(data, i, 'data was same');
+      t.assert.equal(data, i, 'data was same');
     }
   });
 
-  t.test('should be able to remove listeners that have not been attached', function (t) {
+  await t.test('should be able to remove listeners that have not been attached', t => {
     const bus = nanobus();
 
-    t.doesNotThrow(function () {
+    t.assert.doesNotThrow(function () {
       bus.removeListener('yay', handler);
     }, 'removes unattched "yay" event');
-    t.doesNotThrow(function () {
+    t.assert.doesNotThrow(function () {
       bus.removeListener('*', handler);
     }, 'removes unattached "*" event');
-    t.end();
 
     function handler() {}
   });
 
-  t.test('should be able to get an array of listeners', function (t) {
+  await t.test('should be able to get an array of listeners', t => {
     t.plan(2);
     const bus = nanobus();
 
     bus.on('foo', bar);
     bus.on('foo', baz);
 
-    t.deepEqual(bus.listeners('foo'), [bar, baz]);
+    t.assert.deepEqual(bus.listeners('foo'), [bar, baz]);
 
     bus.on('*', bar);
     bus.on('*', baz);
 
-    t.deepEqual(bus.listeners('foo'), [bar, baz]);
+    t.assert.deepEqual(bus.listeners('foo'), [bar, baz]);
 
     function bar(_data) {}
     function baz(_data) {}
   });
 
-  t.test('should be able to trigger multiple listeners with same args', function (t) {
+  await t.test('should be able to trigger multiple listeners with same args', t => {
     t.plan(6);
     const obj = { foo: 'bar' };
     const bus = nanobus();
 
     bus.on('foo', function (data) {
-      t.deepEqual(data, obj);
+      t.assert.deepEqual(data, obj);
     });
 
     bus.on('foo', function (data) {
-      t.deepEqual(data, obj);
+      t.assert.deepEqual(data, obj);
     });
 
     bus.on('foo', function (data) {
-      t.deepEqual(data, obj);
+      t.assert.deepEqual(data, obj);
     });
 
     bus.on('*', function (_name, data) {
-      t.deepEqual(data, obj);
+      t.assert.deepEqual(data, obj);
     });
 
     bus.on('*', function (_name, data) {
-      t.deepEqual(data, obj);
+      t.assert.deepEqual(data, obj);
     });
 
     bus.on('*', function (_name, data) {
-      t.deepEqual(data, obj);
+      t.assert.deepEqual(data, obj);
     });
 
     bus.emit('foo', obj);
